2004d672bddb9064c83db1dab34647210aadb91b
Alexander Vincent <AlexV9582@yahoo.com>
2017-07-13 18:33:06 -0400

    broke stuff and fixed stuff

---
+++ assets/app.js
@@ -0,0 +1,69 @@
+var config = {
+	apiKey: "AIzaSyAUpEMzWj02-qGfXY862k_kM9wDw2dYwVU",
+	authDomain: "homework-stuff.firebaseapp.com",
+	databaseURL: "https://homework-stuff.firebaseio.com",
+	projectId: "homework-stuff",
+	storageBucket: "homework-stuff.appspot.com",
+	messagingSenderId: "1060355102489"
+};
+firebase.initializeApp(config);
+var database = firebase.database()
+var trainName;
+var destination;
+var originTime;
+var frequency;
+
+database.ref().on("value", function(snapshot) {
+	if (snapshot.child("trainName").exists() && snapshot.child("destination").exists() && snapshot.child("originTime").exists() && snapshot.child("frequency").exists()) {
+	    trainName = snapshot.val().trainName;
+	    destination = snapshot.val().destination;
+	    originTime = snapshot.val().originTime;
+	    frequency = snapshot.val().frequency;
+
+    // Append to p element to reflect the local value in firebase.
+	    $("#last-train-name").html(trainName);
+	    $("#last-train-destination").html(destination);
+	    $("#last-train-time").html(originTime);
+	    $("#last-train-frequency").html(frequency);
+
+    // Log the local data to the console.
+	    console.log(trainName);
+	    console.log(destination);
+	    console.log(originTime);
+	    console.log(frequency);
+
+    //Send any existing user entered data to the screen
+	    $("#train-name").html(trainName)
+	    $("#destination").html(destination)
+	    $("#origin-time").html(originTime)
+	    $("#frequency").html(frequency) 
+  }
+})
+  
+  //Clear Firebase
+  $("#clear-button").on("click", function(){
+  	database.ref().set({})
+  })
+  	
+
+
+  // Get user input from form and add to Firebase
+
+$("#submit-button").on("click", function(){	
+	database.ref().push({
+		trainName: $("#train-name-entry").val().trim(),
+		destination: $("#destination-entry").val().trim(),
+		originTime: $("#origin-time-entry").val().trim(),
+		frequency: $("#frequency-entry").val().trim()
+	})
+
+// Display firebase data in panels
+	$("#last-train-name").html(trainName);
+    $("#last-train-destination").html(destination);
+    $("#last-train-time").html(originTime);
+    $("#last-train-frequency").html(frequency);
+	
+})
+
+
+
